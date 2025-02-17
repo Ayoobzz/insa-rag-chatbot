a5490d7aea51c6f27833107c4bb9f23d884d9936
Ayoobe <ayoobakremi@gmail.com>
2025-02-17 01:41:47 +0100

    added chunking code

--- nlp/semantic_filter.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-from sklearn.metrics.pairwise import cosine_similarity
-
-model = SentenceTransformer("all-MiniLM-L6-v2")
-
-def is_redundant(text, existing_embeddings, threshold=0.85):
-    new_embedding = model.encode([text])
-    similarities = cosine_similarity(new_embedding, existing_embeddings)
-    return similarities.max() > threshold
